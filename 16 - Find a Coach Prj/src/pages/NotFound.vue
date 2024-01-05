a8669dd5d23b0972db0933017220139d29612587
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-01-05 16:01:38 +0330

    Update NotFound.vue

--- 16 - Find a Coach Prj/src/pages/NotFound.vue
+++ 16 - Find a Coach Prj/src/pages/NotFound.vue
@@ -1,3 +1,11 @@
 <template>
-    <section></section>
+    <section>
+        <BaseCard>
+            <h2>Page not found</h2>
+            <p>
+                This page could not be found - maybe check out all our
+                <RouterLink to="/coaches">coaches</RouterLink>.
+            </p>
+        </BaseCard>
+    </section>
 </template>
