9fe59c4f5aec4268c4fd7396bfea17e5a1c8eb13
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-01-06 16:22:01 +0330

    Update CoachDetail.vue

--- 16 - Find a Coach Prj/src/pages/coaches/CoachDetail.vue
+++ 16 - Find a Coach Prj/src/pages/coaches/CoachDetail.vue
@@ -1,25 +1,27 @@
 <template>
-  <section>
-    <BaseCard>
-      <h2>{{ fullName }}</h2>
-      <h3>${{ rate }}/hour</h3>
-    </BaseCard>
-  </section>
-  <section>
-    <BaseCard>
-      <header>
-        <h2>Interested? Reach out now!</h2>
-        <BaseButton link :to="contactLink">Contact</BaseButton>
-      </header>
-      <RouterView />
-    </BaseCard>
-  </section>
-  <section>
-    <BaseCard>
-      <BaseBadge v-for="area in areas" :key="area" :type="area" :title="area"></BaseBadge>
-      <p>{{ description }}</p>
-    </BaseCard>
-  </section>
+  <div>
+    <section>
+      <BaseCard>
+        <h2>{{ fullName }}</h2>
+        <h3>${{ rate }}/hour</h3>
+      </BaseCard>
+    </section>
+    <section>
+      <BaseCard>
+        <header>
+          <h2>Interested? Reach out now!</h2>
+          <BaseButton link :to="contactLink">Contact</BaseButton>
+        </header>
+        <RouterView />
+      </BaseCard>
+    </section>
+    <section>
+      <BaseCard>
+        <BaseBadge v-for="area in areas" :key="area" :type="area" :title="area"></BaseBadge>
+        <p>{{ description }}</p>
+      </BaseCard>
+    </section>
+  </div>
 </template>
 
 
