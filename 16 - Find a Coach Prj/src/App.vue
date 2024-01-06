22643ad2bc25aae8fca69d2369eebdd118a723cd
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-01-06 19:12:33 +0330

    Update App.vue

--- 16 - Find a Coach Prj/src/App.vue
+++ 16 - Find a Coach Prj/src/App.vue
@@ -1,15 +1,35 @@
 <template>
     <TheHeader />
-    <RouterView />
+    <RouterView v-slot="slotProps">
+        <transition name="route" mode="out-in">
+            <component :is="slotProps.Component"></component>
+        </transition>
+    </RouterView>
+    
 </template>
 
 
 
 <script>
     import TheHeader from './components/layout/TheHeader.vue'
-
+    
     export default {
-        components: { TheHeader }
+        components: { TheHeader },
+        computed: {
+            didAutoLogout() {
+                return this.$store.getters.didAutoLogout
+            }
+        },
+        created() {
+            this.$store.dispatch('tryLogin')
+        },
+        watch: {
+            didAutoLogout(current, old) {
+                if (current && current !== old){
+                    this.$router.replace('./coaches')
+                }
+            }
+        }
     }
 </script>
 
@@ -20,4 +40,24 @@
     * { box-sizing: border-box; }
     html { font-family: "Roboto", sans-serif; }
     body { margin: 0; }
+
+    .route-enter-from {
+        opacity: 0;
+        transform: translateY(-30px);
+    }
+    .route-leave-to {
+        opacity: 0;
+        transform: translateY(30px);
+    }
+    .route-enter-active {
+        transition: all 0.3s ease-out;
+    }
+    .route-leave-active {
+        transition: all 0.3s ease-in;
+    }
+    .route-enter-to,
+    .route-leave-from {
+        opacity: 1;
+        transform: translateY(0);
+    }
 </style>
