ecabdbc7a6e93b2d9740a6b942d4998d6b552400
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-01-06 19:10:12 +0330

    Add files via upload

---
+++ 16 - Find a Coach Prj/src/pages/auth/UserAuth.vue
@@ -0,0 +1,126 @@
+<template>
+  <div>
+    <BaseDialog :show="!!error" @close="handleErr"
+        title="An  error occurred." fixed>
+      <p>{{ error }}</p>
+    </BaseDialog>
+    <BaseDialog :show="isLoading"
+        title="Authenticating..." fixed>
+      <BaseSpinner />
+    </BaseDialog>
+    <BaseCard>
+      <form @submit.prevent="submitForm">
+        <div class="formControl">
+          <label for="email">E-Mail</label>
+          <input type="email" id="email" v-model.trim="email" />
+        </div>
+        <div class="formControl">
+          <label for="password">Password</label>
+          <input type="password" id="password" v-model.trim="password" />
+        </div>
+        <p class="err" v-if="!formIsValid">
+          Please enter a valid email and password (At least 6 characters long).
+        </p>
+        <BaseButton>{{ submitButtonCaption }}</BaseButton>
+        <BaseButton type="button" mode="flat" @click="switchAuthMode">{{ switchModeButtonCaption }}</BaseButton>
+      </form>
+    </BaseCard>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      formIsValid: true,
+      mode: 'login',
+      isLoading: false,
+      error: null
+    }
+  },
+  computed: {
+    submitButtonCaption() {
+      if (this.mode === 'login') { return 'Login' }
+      else { return 'Signup' }
+    },
+    switchModeButtonCaption() {
+      if (this.mode === 'login') { return 'Signup instead' }
+      else { return 'Login instead' }
+    }
+  },
+  methods: {
+    async submitForm() {
+      this.formIsValid = true;
+      if ( this.email === '' ||
+        !this.email.includes('@') ||
+        this.password.length < 6)
+      {
+        this.formIsValid = false
+        return
+      }
+
+      this.isLoading = true
+      const actionPayload = {
+        email: this.email,
+        password: this.password
+      }
+
+      try {
+        if (this.mode === 'login'){
+          await this.$store.dispatch('login', actionPayload)
+        }
+        else {
+          await this.$store.dispatch('signup', actionPayload)
+        }
+
+        const redirectUrl = '/' + (this.$route.query.redirect || 'coaches')
+        this.$router.replace(redirectUrl)
+      }
+      catch(err) {
+        this.error = err.message || 'Failed, try later. '
+      }
+
+      this.isLoading = false
+    },
+    switchAuthMode() {
+      if (this.mode === 'login') { this.mode = 'signup' }
+      else { this.mode = 'login' }
+    },
+    handleErr() {
+      this.error = null
+    }
+  }
+}
+</script>
+
+<style scoped>
+  form {
+    margin: 1rem;
+    padding: 1rem;
+  }
+    .formControl { margin: 0.5rem 0; }
+
+  label {
+    font-weight: bold;
+    margin-bottom: 0.5rem;
+    display: block;
+  }
+
+  input,
+  textarea {
+    display: block;
+    width: 100%;
+    font: inherit;
+    border: 1px solid #ccc;
+    padding: 0.15rem;
+  }
+    input:focus,
+    textarea:focus {
+      border-color: #3d008d;
+      background-color: #faf6ff;
+      outline: none;
+    }
+  .err { color: red; }
+</style>
