4c5d7e9165a2363abe42ca507d8ee357f2f4225f
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-01-06 19:10:48 +0330

    Update CoachesList.vue

--- 16 - Find a Coach Prj/src/pages/coaches/CoachesList.vue
+++ 16 - Find a Coach Prj/src/pages/coaches/CoachesList.vue
@@ -1,96 +1,97 @@
 <template>
-  <div>
-    <base-dialog :show="!!error" title="An error occurred!" @close="handleError">
-      <p>{{ error }}</p>
-    </base-dialog>
-    <section>
-      <CoachFilter @change-filter="setFilters"></CoachFilter>
-    </section>
-    <section>
-      <BaseCard>
-        <div class="controls">
-          <BaseButton mode="outline" @click="loadCoaches(true)">Refresh</BaseButton>
-          <BaseButton v-if="!isCoach && !isLoading" link to="/register">Register as Coach</BaseButton>
-        </div>
-        <div v-if="isLoading">
-          <baseSpinner />
-        </div>
-        <ul v-else-if="hasCoaches">
-          <CoachItem
-            v-for="coach in filteredCoaches"
-            :key="coach.id"
-            :id="coach.id"
-            :first-name="coach.firstName"
-            :last-name="coach.lastName"
-            :rate="coach.hourlyRate"
-            :areas="coach.areas"
-          ></CoachItem>
-        </ul>
-        <h3 v-else>No coaches found.</h3>
-      </BaseCard>
-    </section>
-  </div>
+    <div>
+        <BaseDialog :show="!!error" title="An error occurred!" @close="handleError">
+            <p>{{ error }}</p>
+        </BaseDialog>
+        <section>
+            <CoachFilter @changeFilter="setFilters" />
+        </section>
+        <section>
+            <BaseCard>
+                <div class="controls">
+                    <BaseButton mode="outline">Refresh</BaseButton>
+                    <BaseButton link to="/auth?redirect=register" v-if="!isLoggedIn">Login to Register as Coach</BaseButton>
+                    <BaseButton link to="/register" v-if="isLoggedIn && !isCoach && !isLoading">Register as Coach</BaseButton>
+                </div>
+                <div v-if="isLoading">
+                    <BaseSpinner />
+                </div>
+                <ul v-if="hasCoaches">
+                    <CoachItem :key="coach.id"
+                        v-for="coach in filterCoaches"
+                        :id="coach.id"
+                        :first-name="coach.firstName"
+                        :last-name="coach.lastName"
+                        :rate="coach.hourlyRate"
+                        :areas="coach.areas"
+                    />
+                </ul>
+                <h3 v-else>No coaches found.</h3>
+            </BaseCard>
+        </section>
+    </div>
 </template>
-  
+
 
 
 <script>
-import CoachItem from '../../components/coaches/CoachItem.vue';
-import CoachFilter from '../../components/coaches/CoachFilter.vue';
+import CoachItem from '../../components/coaches/CoachItem.vue'
+import CoachFilter from '../../components/coaches/CoachFilter.vue'
 
 export default {
     components: { CoachItem, CoachFilter },
     data() {
         return {
-          isLoading: false,
-          error: null,
-          activeFilters: { frontend: true, backend: true, career: true }
+            activeFilters: { frontend: true, backend: true, career: true }
         }
     },
     computed: {
-        isCoach() {
-          return this.$store.getters['coaches/isCoach']
-        },
-        filteredCoaches() {
-          const coaches = this.$store.getters['coaches/coaches']
-          return coaches.filter(coach => {
-            if (this.activeFilters.frontend && coach.areas.includes('frontend')) {
-              return true
-            }
-            if (this.activeFilters.backend && coach.areas.includes('backend')) {
-              return true
-            }
-            if (this.activeFilters.career && coach.areas.includes('career')) {
-              return true
-            }
-            return false
-          })
+        filterCoaches() {
+            const coaches = this.$store.getters['coaches/coaches']
+            return coaches.filter(coach => {
+                if (this.activeFilters.frontend && coach.areas.includes('frontend')) {
+                    return true
+                }
+                if (this.activeFilters.backend && coach.areas.includes('backend')) {
+                    return true
+                }
+                if (this.activeFilters.career && coach.areas.includes('career')) {
+                    return true
+                }
+                return false
+            })
         },
         hasCoaches() {
-          return !this.isLoading && this.$store.getters['coaches/hasCoaches']
+            return this.$store.getters['coaches/hasCoaches']
         },
+        isCoach() {
+            return this.$store.getters['coaches/isCoach']
+        },
+        isLoggedIn() {
+            return this.$store.getters.isAuthed
+        }
     },
     created() {
-        this.loadCoaches();
+        this.loadCoaches()
     },
     methods: {
         setFilters(updatedFilters) {
-          this.activeFilters = updatedFilters
+            this.activeFilters = updatedFilters
         },
         async loadCoaches(refresh = false) {
-          this.isLoading = true
-          try {
-            await this.$store.dispatch('coaches/loadCoaches', {
-              forceRefresh: refresh
-            })
-          }
-          catch (error) {
-            this.error = error.message || 'Something went wrong!'
-          }
-          this.isLoading = false
+            this.isLoading = true
+            try {
+                await this.$store.dispatch('coaches/loadCoaches', {
+                forceRefresh: refresh
+                })
+            }
+            catch (error) {
+                this.error = error.message || 'Something went wrong!'
+            }
+            this.isLoading = false
         },
         handleError() {
-          this.error = null
+            this.error = null
         }
     }
 }
@@ -98,15 +99,14 @@ export default {
 
 
 
-<style scoped>
-  ul {
-    list-style: none;
-    margin: 0;
-    padding: 0;
-  }
-
-  .controls {
-    display: flex;
-    justify-content: space-between;
-  }
+<style>
+    ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
+    .controls {
+        display: flex;
+        justify-content: space-between;
+    }
 </style>
