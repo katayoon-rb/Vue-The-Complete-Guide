d78a39d6bf92dc9ba510cd919857008e35de08d8
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-01-17 17:45:14 +0330

    Add files via upload

---
+++ 14 Animations and Transitions/src/components/BaseModal.vue
@@ -0,0 +1,65 @@
+<template>
+  <div v-if="open" class="backdrop" @click="$emit('close')"></div>
+  <Transition name="modal">
+    <dialog open v-if="open">
+      <slot></slot>
+    </dialog>
+  </Transition>
+  
+</template>
+
+<script>
+  export default {
+    props: ['open'],
+    emits: ['close']
+  }
+</script>
+
+<style scoped>
+  .backdrop {
+    z-index: 10;
+    position: fixed;
+    top: 0;
+    left: 0;
+
+    width: 100%;
+    height: 100vh;
+    background-color: rgba(0, 0, 0, 0.75);
+  }
+
+  dialog {
+    z-index: 100;
+    position: fixed;
+    top: 30vh;
+    left: calc(50% - 15rem);
+
+    width: 30rem;
+    background-color: white;
+
+    margin: 0;
+    padding: 1rem;
+
+    border: none;
+    border-radius: 12px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  }
+
+
+  /* Modal transition component */
+  .modal-enter-active {
+    animation: modal .3s ease-out;
+  }
+  .modal-leave-active {
+    animation: modal .3s ease-out reverse;
+  }
+  @keyframes modal {
+    from {
+      opacity: 0;
+      transform: translateY(-50px) scale(.9);
+    }
+    to {
+      opacity: 1;
+      transform: translateY(0) scale(1);
+    }
+  }
+</style>
---
+++ 14 Animations and Transitions/src/components/TheSamesies.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="same">
+        <slot></slot>
+    </div>
+</template>
+
+
+
+<script>
+    export default { }
+</script>
+
+
+
+<style scoped>
+    div {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
